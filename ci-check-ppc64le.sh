d00b42ff7c53efe785ddf0174e5431b59ce5b05e
Prajyot-Parab <42934355+Prajyot-Parab@users.noreply.github.com>
2019-07-18 13:56:50 +0530

    Delete ci-check-ppc64le.sh

--- ci-check-ppc64le.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# If we're doing push build, as opposed to a PR, always run make ci
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    docker login --username prajyotparab1904 --password 4docker4
-    docker build -t prajyotparab1904/push-workflow-example:latest -f Dockerfile.ppc64le .
-    docker push prajyotparab1904/push-workflow-example:latest
-fi
